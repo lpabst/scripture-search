6e2d9f1033948e07328ddac4e466df407109a036
Loren Pabst <lorenpabst@Lorens-MacBook-Pro.local>
2022-05-30 16:35:07 -0600

    small updates

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,28 +1,63 @@
-import express, { Request, Response, NextFunction } from 'express';
+import { celebrate, Joi } from "celebrate";
+import express, { Request, Response, NextFunction } from "express";
 
 const userController = express.Router();
 
-userController.post('/user', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await req.ctx!.services.user.createUser({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-    })
-    return res.sendStatus(204);
-  } catch(e) {
-    next(e);
+userController.post(
+  "/user",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await req.ctx!.services.user.createUser({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      return res.sendStatus(204);
+    } catch (e) {
+      next(e);
+    }
   }
-})
+);
 
-userController.get('/user', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const user = await req.ctx?.services.user.getUserById(req.userId!);
-    return res.status(200).send(user);
-  } catch(e) {
-    next(e);
+userController.get(
+  "/user",
+  celebrate({
+    headers: Joi.object().keys({
+      authorization: Joi.string().required(),
+    }),
+  }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await req.ctx?.services.user.getUserById(req.userId!);
+      return res.status(200).send(user);
+    } catch (e) {
+      next(e);
+    }
   }
-})
+);
+
+userController.get(
+  "/email/verify",
+  celebrate({
+    params: Joi.object().keys({
+      authorization: Joi.string().required(),
+    }),
+  }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // verify email with token here
+      // return res.status(200).send(user);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
 
 export default userController;
